0effe5c43d6fa4086976d6aedc0c33809fedcf80
Isa GT <igrahamtorres@smith.edu>
2026-09-20 17:05:51 -0400

    Re-formatted code to be within the main function.

--- hw01.py
+++ hw01.py
@@ -1,5 +1,5 @@
 # ------------------------------------------------------
-#        Name: (put your name here)
+#        Name: Isa Graham Torres
 #       Peers: (add any collaborators)
 #  References: (anything you checked to solve this)
 # ------------------------------------------------------
@@ -45,20 +45,23 @@ def main():
     # Part 1: Basic Operations
     # =============================================
     # Your code for part 1 under this line and before the print statements
+    x = 27
+    y = 1
+    a = 1.5
+    b = 7
+    c = -1
+    #I define my variable's values:
+    
+    #I set my result equal to the division equation
+    result = ((3*x)-(9*y))/((2*a)*(b-c))
 
-x = 27
-y = 1
-a = 1.5
-b = 7
-c = -1
-result = ((3*x)-(9*y))/((2*a)*(b-c))
-
-print("Part 1: x =", x)
-print("Part 1: y =", y)
-print("Part 1: a =", a)
-print("Part 1: b =", b)
-print("Part 1: c =", c)
-print("Part 1: result =", result)
+#I print the variable's values and the final result
+    print("Part 1: x =", x)
+    print("Part 1: y =", y)
+    print("Part 1: a =", a)
+    print("Part 1: b =", b)
+    print("Part 1: c =", c)
+    print("Part 1: result =", result)
 
     # End of Part 1 ----------------------
 
@@ -66,14 +69,18 @@ print("Part 1: result =", result)
     # Part 2: Power
     # =============================================
     # Your code for part 2 under this line and before the print statements
-
-x = 5
-y = -3
-result2 = (x**2)*(y**4)
-
-print("Part 2: x =", x)
-print("Part 2: y =", y)
-print("Part 2: result =", result2)
+    
+    #I define the variable's values
+    x = 5
+    y = -3
+    
+    #I set my result2 equal to the equation
+    result2 = (x**2)*(y**4)
+    
+    #I print my variable's values and the equation's results
+    print("Part 2: x =", x)
+    print("Part 2: y =", y)
+    print("Part 2: result =", result2)
 
     # End of Part 2 ----------------------
 
@@ -81,16 +88,18 @@ print("Part 2: result =", result2)
     # Part 3: Integer divide
     # =============================================
     # Your code for part 3 under this line and before the print statements
-
-
-a = 100
-b = 13
-
-result = 100 // 13
-
-print("Part 3: a =", a)
-print("Part 3: b =", b)
-print("Part 3: result =", result)
+    
+    #I define my variable's values
+    a = 100
+    b = 13
+    
+    #I set my result equal to the equation (using // to get an int)
+    result = 100 // 13
+    
+    #I print the variable's values and the result of the equation
+    print("Part 3: a =", a)
+    print("Part 3: b =", b)
+    print("Part 3: result =", result)
 
     # End of Part 3 ----------------------
 
@@ -98,11 +107,13 @@ print("Part 3: result =", result)
     # Part 4: Modulo
     # =============================================
     # Your code for part 4 under this line and before the print statements
-
-
-result4 = 100 % 13
-
-print("Part 4: result =", result4)
+    
+    #I don't need to define new variables as I'm using the same from Part 3
+    #I set my result equal to the equation (using % to get the remainder)
+    result4 = 100 % 13
+    
+    #I print the equation's result
+    print("Part 4: result =", result4)
 
 
 
